fc5b112d952bdf1c3ac5c36254912bfb092dc2f4
FF2L <94149648+FF2L@users.noreply.github.com>
2025-04-11 21:03:15 +0700

    Thêm phần thu hồi tin nhắn với 1(chỉ thu hồi với chính mình) 2(xóa tin nhắn) thêm phần hướng dẫn sử dụng trong routes

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -7,6 +7,10 @@ const messageController = {}
 messageController.create = async(req,res) =>{
    try
    { const {chatId , content} = req.body
+
+   if(! chatId){
+        return res.status(400).json({msg: 'Thiếu chatRoom Id'})
+   }
     const userId = req.user._id // lấy tự middleware
 
     const message = new Message({
@@ -50,17 +54,71 @@ messageController.create = async(req,res) =>{
 messageController.getAll = async(req, res) =>{
     try{
         const {chatId} = req.body
+        
         if(!chatId){
-            return res.status(400).json({msg: 'Thiếu chat Id'})
+            return res.status(400).json({msg: 'Thiếu chatRoom Id'})
         }
         const messages = await Message.find({chatId}) // Chỉ nhận object key value nếu truyền vào chatId = value thoi nên ko dc
         .sort({ createdAt: 1}) //Sắp xếp từ tạo lâu nhất đến mới nhất tăng dần thời gian
         .populate('sendID', 'name email')
+
+        if(!messages){
+            return res.status(404).json({msg: `Lỗi ko tìm thấy phòng chat ${chatId}`})
+        }
+
         return res.status(200).json(messages)
     }catch(err){
         console.log('Lỗi lấy tất cả tin nhắn', err)
         return res.status(500).json({msg: 'Lỗi lấy tất cả tin nhắn'})
     }
+}
+
+//Thu hồi tin nhắn cần cung cấp 
+messageController.recall = async(req,res) =>{
+    try{
+        //id của message nhận từ client
+        const {_id} = req.body
+        const {code} = req.params
+        //Nhận từ authorMiddleware
+        const userRecallId = req.user._id 
+
+        //Lấy thông tin message bằng _id của message đã lấy từ body và kiểm tra xem có thuộc về người gọi api này koko
+        const messageBelongUserId = await Message.findById(_id).populate({
+            path: 'chatId',
+            populate: {path: 'members',select: 'name email' }
+        })
+        if(!messageBelongUserId){
+            return res.status(400).json({msg : 'Tin nhắn này không tồn'})
+        }
+        //Kiểm tra ko thuộc về thì trả lỗi và kết thúc
+        if(messageBelongUserId.sendID.toString() !== userRecallId.toString())
+            return res.status(400).json({msg:'Tin nhắn không thuộc về người thu hồi'})
+        
+        //Kiểm tra xem đã thu hồi chưa
+        if(messageBelongUserId.recall.toString() === '1' || messageBelongUserId.recall.toString()==='2'){
+            return res.status(400).json({msg: 'Tin nhắn đã bị thu hồi trước đó'})
+        }
+
+        //code 1 là chỉ với người thu hồi còn 2 là tất cả coi như xóa nếu xóa thì thông báo tin nhắn đã thu hồi
+        if(code.toString() === '1'){
+            //Mặc định update không trả về document thêm {new:true} mới trả về doccument
+            const recallMessage = await Message.findByIdAndUpdate(_id, {content: '' , recall:'1'}, {new: true})
+            return res.status(200).json(recallMessage)
+        }else {
+            const recallMessage = await Message.findByIdAndUpdate(_id, {content: '' , recall:'2'}, {new: true})
+            const members = messageBelongUserId.chatId.members
+            members.forEach(mem => {
+                if(mem._id.toString() !== userRecallId.toString())
+                    req.io.to(mem._id.toString()).emit('recall',recallMessage)
+            })
+            return res.status(200).json(recallMessage)
+        }
+
+    }catch(err){
+        console.log('Lỗi thu hồi tin nhắn',err)
+        return res.status(500).json({msg: 'Lỗi thu hồi tin nhắn'})
+    }
+
 }
 
 
--- backend/models/Message.js
+++ backend/models/Message.js
@@ -4,6 +4,7 @@ const MessageSchema = new mongoose.Schema({
     chatId: {type: mongoose.Schema.Types.ObjectId, ref: "Chatroom"},
     sendID: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
     content:{type: String,require: true},
+    recall: {type: String, default: '0'}
     // seen: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}]
 }, { timestamps: true }); //Lưu thời gian tạo và cập nhật
 
